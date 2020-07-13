d07c8d41d50ae342e0aa5fc71d60854abebf688f
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-07-12 21:45:39 -0400

    Add login functionality to index.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 <div class="container">
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }} <span class="caret"></span>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </div>
+    </li>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
