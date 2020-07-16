dee6cb49125c344695b8e1b8fd8b484151356f79
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-07-16 19:55:50 -0400

    Update carousel_index and index.blade.php

--- resources/views/components/carousel_index.blade.php
+++ resources/views/components/carousel_index.blade.php
@@ -1,3 +1,35 @@
-<div>
-
-</div>
+<!-- CAROUSEL -->
+<header id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div  class="carousel-inner carousel__height">
+        <figure class="carousel-item active">
+            <img src="img/car.jpg" class="d-block w-100 carousel__height--img" alt="car">
+            <figcaption class="carousel-caption mb-5">
+                <h1 class="carousel__heading text-center">Konti tracker is now available.</h1>
+                <p class="carousel__text lead">Keep track of all your PTO usage</p>
+            </figcaption>
+        </figure>
+        <figure class="carousel-item">
+            <img src="img/dashboard.jpg" class="d-block w-100 carousel__height--img" alt="motor">
+            <figcaption class="carousel-caption mb-5">
+                <h1 class="carousel__heading text-center">Peace of mind</h1>
+                <p class="carousel__text lead">All PTO information can be tracked here</p>
+            </figcaption>
+        </figure>
+        <figure class="carousel-item">
+            <img src="img/vacation.jpg" class="d-block w-100 carousel__height--img"  alt="vacation">
+            <figcaption class="carousel-caption mb-5">
+                <h1 class="carousel__heading">Enjoy life</h1>
+                <p class="carousel__text lead">Save your PTO days for your next beautiful vacation.</p>
+            </figcaption>
+        </figure>
+    </div>
+    <!-- CAROUSEL CONTROLS -->
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next actvie" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon active" aria-hidden="true"></span>
+        <span class="sr-only active">Next</span>
+    </a>
+</header>
