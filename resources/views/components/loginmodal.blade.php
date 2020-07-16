ebb15cbfc54960c1b43a86d344d11b4c61a8a261
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-07-16 19:50:14 -0400

    Create new carousel_index component

--- resources/views/components/loginmodal.blade.php
+++ resources/views/components/loginmodal.blade.php
@@ -1,3 +1,46 @@
-<div>
+ <!-- MODAL -->
+    <section class="modal" id="exampleModalCentered" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header modal__header">
+                    <h5 class="modal-title" id="exampleModalCenteredLabel">Sign In</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body modal__body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="email">{{ __('E-Mail Address') }}</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-</div>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input id="password" type="password" class="form-control" @error('password') is-invalid @enderror name="password" required autocomplete="current-password">
+
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                        <a href="#">Forgot password/username?</a>
+                        <div class="mt-2 d-flex justify-content-end">
+                            <button type="button" class="btn btn-secondary mr-2 modal__button" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary modal__button">
+                                {{ __('Login') }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+</section>
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,51 +9,9 @@
 @stop
 
 @section('body-content')
-    <!-- MODAL -->
-    <section class="modal" id="exampleModalCentered" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header modal__header">
-                    <h5 class="modal-title" id="exampleModalCenteredLabel">Sign In</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body modal__body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="email">{{ __('E-Mail Address') }}</label>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input id="password" type="password" class="form-control" @error('password') is-invalid @enderror name="password" required autocomplete="current-password">
+    <x-loginmodal></x-loginmodal>
 
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
-                        <a href="#">Forgot password/username?</a>
-                        <div class="mt-2 d-flex justify-content-end">
-                            <button type="button" class="btn btn-secondary mr-2 modal__button" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary modal__button">
-                                {{ __('Login') }}
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 col-md-11 mx-auto mb-5">
