8cea33ca05c154a47b663727e1e7f9ea1259666c
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-07-16 20:13:41 -0400

    Update konti-cards and index.php

--- resources/views/components/konti-cards.blade.php
+++ resources/views/components/konti-cards.blade.php
@@ -1,3 +1,45 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-</div>
+<!-- KONTI-CARDS -->
+<section class="container-fluid">
+    <div class="row d-flex justify-content-between">
+        <div class="col-md-5">
+            <div class="konti-cards card card-animation__left">
+                <img class="card-img-top konti-card__img my-3 w-25 mx-auto"  src="svg/car-parts.svg" alt="tracker">
+                <div class="card-body">
+                    <h4 class="card-title">PTO Tracking software</h4>
+                    <p class="card-text">
+                        Your attendance, years of service, scheduled/unschduled time all tracked in one location.
+                    </p>
+                </div>
+            </div>
+            <div class="konti-cards card card-animation__left">
+                <img class="card-img-top konti-card__img mx-auto my-3 w-25" src="svg/brake-disc.svg" alt="brake disc">
+                <div class="card-body">
+                    <h4 class="card-title">No more PTO/attendance sheet request</h4>
+                    <p class="card-text">
+                        Put a <span class="font-weight-bolder">stop</span> on tracking everything on paper. Let the Konti-Tracker do all the work.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-5 my-5 my-sm-1 ">
+            <div class="konti-cards card card-animation__right">
+                <img class="card-img-top konti-card__img my-4  w-25 mx-auto"  src="svg/graph.svg" alt="graph">
+                <div class="card-body">
+                    <h4 class="card-title display-5">Graphs</h4>
+                    <p class="card-text">
+                        Beautifully designed graphs to view all your customized data.
+                    </p>
+                </div>
+            </div>
+            <div class="konti-cards card card-animation__right">
+                <img class="card-img-top konti-card__img my-3 w-25 mx-auto"  src="svg/car-parts.svg" alt="tracker">
+                <div class="card-body">
+                    <h4 class="card-title">PTO Tracking software</h4>
+                    <p class="card-text text-dark">
+                        Your attendance, years of service, scheduled/unschduled time all tracked in one location.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,59 +9,20 @@
 @stop
 
 @section('body-content')
+
     <x-loginmodal></x-loginmodal>
 
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 col-md-11 mx-auto mb-5">
+
                 <x-carousel_index></x-carousel_index>
+
                 <x-pto_jumbotron></x-pto_jumbotron>
 
-                <section class="container-fluid">
-                    <div class="row d-flex justify-content-between">
-                        <div class="col-md-5">
-                                <!-- KONTI-CARDS -->
-                            <div class="konti-cards card card-animation__left">
-                                <img class="card-img-top konti-card__img my-3 w-25 mx-auto"  src="svg/car-parts.svg" alt="tracker">
-                                <div class="card-body">
-                                    <h4 class="card-title">PTO Tracking software</h4>
-                                    <p class="card-text">
-                                        Your attendance, years of service, scheduled/unschduled time all tracked in one location.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="konti-cards card card-animation__left">
-                                <img class="card-img-top konti-card__img mx-auto my-3 w-25" src="svg/brake-disc.svg" alt="brake disc">
-                                <div class="card-body">
-                                    <h4 class="card-title">No more PTO/attendance sheet request</h4>
-                                    <p class="card-text">
-                                        Put a <span class="font-weight-bolder">stop</span> on tracking everything on paper. Let the Konti-Tracker do all the work.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-5 my-5 my-sm-1 ">
-                            <div class="konti-cards card card-animation__right">
-                                <img class="card-img-top konti-card__img my-4  w-25 mx-auto"  src="svg/graph.svg" alt="graph">
-                                <div class="card-body">
-                                    <h4 class="card-title display-5">Graphs</h4>
-                                    <p class="card-text">
-                                        Beautifully designed graphs to view all your customized data.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="konti-cards card card-animation__right">
-                                <img class="card-img-top konti-card__img my-3 w-25 mx-auto"  src="svg/car-parts.svg" alt="tracker">
-                                <div class="card-body">
-                                    <h4 class="card-title">PTO Tracking software</h4>
-                                    <p class="card-text text-dark">
-                                        Your attendance, years of service, scheduled/unschduled time all tracked in one location.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <x-konti-cards></x-konti-cards>
+
+
                 <!-- FOOTER -->
                 <footer class="footer d-flex flex-wrap align-content-end">
                     <div class="container">
