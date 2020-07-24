35ddb7acb6e82407eb1d71ffedeae671941f0394
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-07-23 22:04:07 -0400

    add AJAX functionality using vue.js to login modal

---
+++ resources/js/components/LoginForm.vue
@@ -0,0 +1,102 @@
+
+<template>
+    <div>
+        <form method="POST" @submit.prevent = 'submitForm'>
+            <div class="form-group">
+                <label for="email">Email Address</label>
+                <input id="email"
+                       v-model="email"
+                       type="email"
+                       class="form-control"
+                       name="email"
+                       autocomplete="email"
+                       autofocus
+                       @keydown="errors.clear('email')">
+                <strong v-text="errors.get('email')" class="help is-danger"></strong>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input id="password"
+                       v-model="password"
+                       type="password"
+                       class="form-control"
+                       name="password"
+                       required
+                       autocomplete="current-password"
+                       @keydown="errors.clear('password')">
+                <strong v-text="errors.get('password')" class="help is-danger"></strong>
+            </div>
+
+            <a href="#">Forgot password/username?</a>
+
+            <div class="mt-2 d-flex justify-content-end">
+                <button type="button" class="btn btn-secondary mr-2 modal__button" data-dismiss="modal">Close</button>
+                <button type="submit"  class="btn btn-primary modal__button">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    /**
+     * Keeps track of all of laravel's validation errors that come from axios.
+     */
+    class Errors {
+        /**
+         * Create a new controller instance.
+         *
+         * @return void
+         */
+        constructor() {
+            this.errors = {};
+        }
+
+        /**
+         * Get error(field) name to display correct error.
+         *
+         * @return object
+         */
+        get(field) {
+            if (this.errors[field]) {
+                return this.errors[field][0];
+            }
+        }
+
+        /**
+         * Get error from axios and store it.
+         *
+         * @return void
+         */
+        record(errors) {
+            this.errors = errors;
+        }
+        /**
+         * Delete error message on keydown.
+         *
+         * @return void
+         */
+        clear(field) {
+            delete this.errors[field];
+        }
+
+    }
+
+    export default {
+        data() {
+            return {
+                email:'',
+                password:'',
+                errors: new Errors()
+            }
+        },
+        methods: {
+            submitForm() {
+                axios.post("/login", this.$data)
+                    .then(response => alert('success'))
+                    .catch(error => this.errors.record(error.response.data.errors))
+            }
+        }
+
+    }
+</script>
--- resources/views/components/loginmodal.blade.php
+++ resources/views/components/loginmodal.blade.php
@@ -1,4 +1,5 @@
  <!-- MODAL -->
+ <div id="app">
     <section class="modal" id="exampleModalCentered" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
@@ -8,39 +9,14 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body modal__body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="email">{{ __('E-Mail Address') }}</label>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <div  class="modal-body modal__body">
+                    @csrf
 
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input id="password" type="password" class="form-control" @error('password') is-invalid @enderror name="password" required autocomplete="current-password">
+                   <login-form></login-form>
 
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
-                        <a href="#">Forgot password/username?</a>
-                        <div class="mt-2 d-flex justify-content-end">
-                            <button type="button" class="btn btn-secondary mr-2 modal__button" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary modal__button">
-                                {{ __('Login') }}
-                            </button>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
-</section>
+    </section>
+</div>
 
