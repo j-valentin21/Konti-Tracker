37ed23d3b57ec1c8c0153481e302fafc963bc9d2
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-08-31 09:13:24 -0400

    Update view components

--- resources/views/components/konti-cards.blade.php
+++ resources/views/components/konti-cards.blade.php
@@ -3,16 +3,16 @@
     <div class="row d-flex justify-content-between">
         <div class="col-md-5">
             <div class="konti-cards card card-animation__left">
-                <img class="card-img-top konti-card__img my-3 w-25 mx-auto"  src="svg/car-parts.svg" alt="tracker">
+                <img class="card-img-top konti-card__img my-3 w-25 mx-auto"  src="{{ asset('svg/car-parts.svg') }}" alt="tracker">
                 <div class="card-body">
                     <h4 class="card-title">PTO Tracking software</h4>
                     <p class="card-text">
-                        Your attendance, years of service, scheduled/unschduled time all tracked in one location.
+                        Your attendance, years of service, scheduled/unscheduled time all tracked in one location.
                     </p>
                 </div>
             </div>
             <div class="konti-cards card card-animation__left">
-                <img class="card-img-top konti-card__img mx-auto my-3 w-25" src="svg/brake-disc.svg" alt="brake disc">
+                <img class="card-img-top konti-card__img mx-auto my-3 w-25" src="{{ asset('svg/brake-disc.svg') }}" alt="brake disc">
                 <div class="card-body">
                     <h4 class="card-title">No more PTO/attendance sheet request</h4>
                     <p class="card-text">
@@ -23,7 +23,7 @@
         </div>
         <div class="col-md-5 my-5 my-sm-1 ">
             <div class="konti-cards card card-animation__right">
-                <img class="card-img-top konti-card__img my-4  w-25 mx-auto"  src="svg/graph.svg" alt="graph">
+                <img class="card-img-top konti-card__img my-4  w-25 mx-auto"  src="{{ asset('svg/graph.svg') }}" alt="graph">
                 <div class="card-body">
                     <h4 class="card-title display-5">Graphs</h4>
                     <p class="card-text">
@@ -32,11 +32,11 @@
                 </div>
             </div>
             <div class="konti-cards card card-animation__right">
-                <img class="card-img-top konti-card__img my-3 w-25 mx-auto"  src="svg/car-parts.svg" alt="tracker">
+                <img class="card-img-top konti-card__img my-3 w-25 mx-auto"  src="{{ asset('svg/brake-disc.svg') }}" alt="tracker">
                 <div class="card-body">
                     <h4 class="card-title">PTO Tracking software</h4>
                     <p class="card-text text-dark">
-                        Your attendance, years of service, scheduled/unschduled time all tracked in one location.
+                        Your attendance, years of service, scheduled/unscheduled time all tracked in one location.
                     </p>
                 </div>
             </div>
