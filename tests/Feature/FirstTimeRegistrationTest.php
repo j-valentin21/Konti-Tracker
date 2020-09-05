02cc9b60b65a6515b95cc958b39f417e97353590
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-09-05 15:38:45 -0400

    Update FTRT.php

--- tests/Feature/FirstTimeRegistrationTest.php
+++ tests/Feature/FirstTimeRegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -24,4 +25,20 @@ class FirstTimeRegistrationTest extends TestCase
 
         $response->assertRedirect('/');
     }
+
+    /**
+     * Guess cannot access build your profile page without being authenticated.
+     *
+     * @return void
+     */
+    /** @test */
+    public function test_Authenticated_User_Cannot_Access_Build_Your_Profile_Page()
+    {
+        $response = $this->actingAs($user = factory(User::class)->create())
+            ->get('/register/build-your-profile');
+
+        $response->assertRedirect('/');
+        $this->assertAuthenticatedAs($user);
+    }
 }
+
