ee23e33b502dbd891cd70bbcf9acab75fa5b42a9
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-09-06 17:51:49 -0400

    Update modal to disable close on screen

--- public/jquery/modal.js
+++ public/jquery/modal.js
@@ -1,5 +1,5 @@
 //OPEN MODAL ON PAGE LOAD
 $(document).ready(function(){
-    $(".bd-example-modal-lg").modal('show');
+    $(".bd-example-modal-lg").modal('show')
 });
 
--- resources/views/auth/profile/build-your-profile.blade.php
+++ resources/views/auth/profile/build-your-profile.blade.php
@@ -5,7 +5,7 @@
 @section('body-content')
     <form action="">
         <div class="container-fluid">
-            <div class="modal fade bd-example-modal-lg profile" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal fade bd-example-modal-lg profile" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-lg modal-dialog-centered">
                     <div class="modal-content">
                         <div class="row">
