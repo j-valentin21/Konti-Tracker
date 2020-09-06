b3d492cbb6fb883e3529814bc6c2d049b75a5ed7
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-09-06 17:27:49 -0400

    Update ProfileController with new middleware

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('firstTimeUser')->nullable();
+            $table->boolean('firstTimeUser')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
