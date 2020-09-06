7e5fb7c285cf585ae6829f6ab08c79b6c37c498d
juelz21 <52730501+juelz21@users.noreply.github.com>
2020-09-06 16:23:17 -0400

    Update modal to launch on page load

--- resources/views/auth/profile/build-your-profile.blade.php
+++ resources/views/auth/profile/build-your-profile.blade.php
@@ -1,62 +1,78 @@
-<button class="btn btn-lg" data-toggle="modal" data-target=".bd-example-modal-lg"></button>
-<form action="">
-    <div class="container-fluid">
-        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="row">
-                        <div class="col-12">
-                            <section class="form__wizard">
-                                <img class="form__wizard__img img-fluid" src="{{ asset('img/limo_car.jpg') }}" alt="">
-                                <h3 class="form__wizard__header">Welcome to Konti-Tracker</h3>
-                            </section>
-                        </div>
-                    </div>
-                    <div class="row p-5">
-                        <div class="col-4">
-                            <ol class="form__wizard__steps ">
-                                <li class="mb-4 form__wizard__steps--active">Build Your Profile</li>
-                                <li class="mb-4">PTO/Points</li>
-                                <li class="mb-4">Confirmation</li>
-                            </ol>
-                        </div>
-                        <div class="col-8">
-                            <div class="form-group mb-4">
-                                <input id="position"
-                                       type="text"
-                                       class="form-input registration__input @error('position') is-invalid @enderror"
-                                       name="position"
-                                       placeholder="What is your position?"
-                                       value="{{ old('position') }}"
-                                       required autocomplete="position"/>
-                                <label for="position" class="registration__label">{{ __('What is your position?') }}</label>
+@extends('layouts.master')
 
-                                @error('position')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
+@section('title', "Build-Your-Profile")
+
+@section('body-content')
+    <form action="">
+        <div class="container-fluid">
+            <div class="modal fade bd-example-modal-lg profile" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="row">
+                            <div class="col-12">
+                                <section class="form__wizard">
+                                    <img class="form__wizard__img img-fluid" src="{{ asset('img/limo_car.jpg') }}" alt="">
+                                    <h3 class="form__wizard__header">Welcome to Konti-Tracker</h3>
+                                </section>
                             </div>
+                        </div>
+                        <div class="row p-5">
+                            <div class="col-4">
+                                <ol class="form__wizard__steps ">
+                                    <li class="mb-4 form__wizard__steps--active">Build Your Profile</li>
+                                    <li class="mb-4">PTO/Points</li>
+                                    <li class="mb-4">Confirmation</li>
+                                </ol>
+                            </div>
+                            <div class="col-8">
+                                <div class="form-group mb-4">
+                                    <input id="position"
+                                           type="text"
+                                           class="form-input registration__input @error('position') is-invalid @enderror"
+                                           name="position"
+                                           placeholder="What is your position?"
+                                           value="{{ old('position') }}"
+                                           required autocomplete="position"/>
+                                    <label for="position" class="registration__label">{{ __('What is your position?') }}</label>
 
-                            <div>
-                                <div class="form-group">
-                                    <label class="form__wizard__label" for="img" class="text-break">{{ __('Upload Image') }}</label>
-                                    <input type="file"  class="form-control-file" name="img" id="img" aria-describedby="fileHelp">
-                                    <small id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</small>
+                                    @error('position')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
                                 </div>
-                            </div>
-                            <div class="text-right">
-                                <button type="submit" class="form__wizard__btn">
-                                    {{ __('Next') }}
-                                </button>
+
+                                <div>
+                                    <div class="form-group">
+                                        <label class="form__wizard__label" for="img" class="text-break">{{ __('Upload Image') }}</label>
+                                        <input type="file"  class="form-control-file" name="img" id="img" aria-describedby="fileHelp">
+                                        <small id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</small>
+                                    </div>
+                                </div>
+                                <div class="text-right">
+                                    <button type="submit" class="form__wizard__btn">
+                                        {{ __('Next') }}
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</form>
+    </form>
+@stop
+@section('body-scripts')
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
+        crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('jquery/modal.js') }}"></script>
+@endsection
+
+
+
 
 
 
