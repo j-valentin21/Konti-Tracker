5733198e660c8284582c138b0f88eebb36303894
phpdev <jvalentin0221@gmail.com>
2020-10-05 21:55:45 -0400

    Add responsive design to all profile pages

--- resources/views/auth/profile/confirmation.blade.php
+++ resources/views/auth/profile/confirmation.blade.php
@@ -5,26 +5,26 @@
 @section('body-content')
     <div class="container-fluid">
         <div class="modal fade bd-example-modal-lg profile" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-dialog modal-lg modal-dialog-centered form__wizard__modal"">
                 <div class="modal-content">
                     <div class="row">
                         <div class="col-12">
                             <section class="form__wizard">
-                                <img class="form__wizard__img img-fluid" src="{{ asset('img/luxury_car.jpg') }}" alt="luxury car">
+                                <img class="form__wizard__header-img" src="{{ asset('img/luxury_car.jpg') }}" alt="luxury car">
                                 <h3 class="form__wizard__header">Welcome to Konti-Tracker</h3>
                             </section>
                         </div>
                     </div>
                     <div class="row p-5">
                         <!-- ========== STEPS ========== -->
-                        <div class="col-4">
+                        <div class="col-sm-4">
                             <ol class="form__wizard__steps">
                                 <li class="mb-4">Build Your Profile</li>
                                 <li class="mb-4">Avatar</li>
                                 <li class="mb-4 form__wizard__steps--active-bottom">Confirmation</li>
                             </ol>
                         </div>
-                        <div class="col-8">
+                        <div class="col-sm-8">
                             <h1>Review Profile Details</h1>
                             <form action="{{ route('confirmation.store') }}" method="POST" >
                                 @csrf
@@ -52,15 +52,17 @@
                                         </tr>
                                     @endif
                                 </table>
-                                <a href="{{ route('profile') }}">
-                                    <button type="button" class="form__wizard__btn form__wizard__btn--orange mr-2 mb-2">Back to Profile</button>
-                                </a>
-                                <a href="{{ route('confirmation.store') }}">
-                                    <button type="submit" class="form__wizard__btn form__wizard__btn--black mr-2 mb-2">Create Profile</button>
-                                </a>
-                                <a href="{{ route('avatar') }}">
-                                    <button type="button" class="form__wizard__btn form__wizard__btn--orange mr-2 mb-2">Back to Avatar</button>
-                                </a>
+                                <div class="text-center text-sm-right">
+                                    <a href="{{ route('profile') }}">
+                                        <button type="button" class="form__wizard__btn form__wizard__btn--orange mr-2 mb-2">Back to Profile</button>
+                                    </a>
+                                    <a href="{{ route('avatar') }}">
+                                        <button type="button" class="form__wizard__btn form__wizard__btn--orange mr-2 mb-2">Back to Avatar</button>
+                                    </a>
+                                    <a href="{{ route('confirmation.store') }}">
+                                        <button type="submit" class="form__wizard__btn form__wizard__btn--black mr-2 mb-2">Create Profile</button>
+                                    </a>
+                                </div>
                             </form>
                         </div>
                     </div>
