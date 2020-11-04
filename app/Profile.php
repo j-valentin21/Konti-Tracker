864fdf05e3d86908e2ff569ef03a326aa9ba7cbb
phpdev <jvalentin0221@gmail.com>
2020-11-04 15:38:01 -0500

    Refactor update method code in dashboard controller

--- app/Profile.php
+++ app/Profile.php
@@ -27,13 +27,31 @@ class Profile extends Model
     {
         return 60*60*24*2;
     }
+    /**
+     * Create an array of months to be stored in the database and return the current month.
+     *
+     * @return string
+     */
+    public function getBarChartMonth()
+    {
+        if ($this->pto_usage === null) {
+            $months = array('Jan' => 0, 'Feb' => 0, 'Mar' => 0, 'Apr' => 0, 'May' => 0, 'June' => 0,
+                'July' => 0, 'Aug' => 0, 'Sept' => 0, 'Oct' => 0, 'Nov' => 0, 'Dec' => 0);
+            $graph_date = $this->updated_at;
+            $graph_month = $graph_date->shortEnglishMonth;
+            $this->pto_usage = $months;
+            $this->save();
+            return $graph_month;
+        }
+        else {
+            $graph_date = $this->updated_at;
+            $graph_month = $graph_date->shortEnglishMonth;
+            return $graph_month;
+        }
+    }
 
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function getColumnNameAttribute($value) {
-        return json_decode($value);
     }
 }
