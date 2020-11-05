d946eaceed77a675856e60c663ce0b235bb671cb
phpdev <jvalentin0221@gmail.com>
2020-11-05 13:48:48 -0500

    Add Get_Bar_Chart_Month test

--- tests/Unit/ProfileTest.php
+++ tests/Unit/ProfileTest.php
@@ -3,10 +3,14 @@
 namespace Tests\Unit;
 
 use App\Profile;
-use PHPUnit\Framework\TestCase;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ProfileTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * Test to check it expireDate method is calculating properly.
      *
@@ -17,5 +21,21 @@ class ProfileTest extends TestCase
         $expireDate = $profile->expireDate();
         $days = 60*60*24*2;
         $this->assertEquals($expireDate, $days);
+    }
+
+    /**
+     * Test to check it expireDate method is calculating properly.
+     *
+     */
+    public function test_Get_Bar_Chart_Month():void
+    {
+        $user = factory(User::class)->create();
+        $profile = factory(Profile::class)->create();
+        $barMonth = $profile->getBarChartMonth();
+
+        $graph_date = $profile->updated_at;
+        $graph_month = $graph_date->shortEnglishMonth;
+
+        $this->assertEquals($barMonth, $graph_month);
     }
 }
