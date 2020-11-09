55081d4509e1080fcf51a7b114090e6c0a426035
phpdev <jvalentin0221@gmail.com>
2020-11-09 12:11:55 -0500

    Add Reset_Months test

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -11,6 +11,7 @@ $factory->define(Profile::class, function (Faker $faker) {
         'pto' => $faker->randomNumber(2),
         'points'=> $faker->randomNumber(2),
         'user_id'=> 1,
-        'pto_usage'=> $faker->randomElements([10,3,8,5,6,7,10,16,11,7,9,1])
+        'pto_usage'=> $faker->randomElements([10,3,8,5,6,7,10,16,11,7,9,1]),
+        'points_usage'=> $faker->randomElements([10,3,8,5,6,7,10,16,11,7,9,1])
     ];
 });
--- tests/Unit/ProfileTest.php
+++ tests/Unit/ProfileTest.php
@@ -4,12 +4,12 @@ namespace Tests\Unit;
 
 use App\Profile;
 use App\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class ProfileTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     /**
      * Test to check it expireDate method is calculating properly.
@@ -37,5 +37,31 @@ class ProfileTest extends TestCase
         $graph_month = $graph_date->shortEnglishMonth;
 
         $this->assertEquals($barMonth, $graph_month);
+    }
+
+    /**
+     * Test to check it expireDate method is calculating properly.
+     *
+     */
+    public function test_Reset_Months():void
+    {
+        $user = factory(User::class)->create();
+        $profile = factory(Profile::class)->create();
+
+        $graph_date = $profile->updated_at;
+        $graph_year = $graph_date->year;
+        $year_count = $profile->user->year_count = $graph_year;
+        $months = array('Jan' => 6, 'Feb' => 4, 'Mar' => 0, 'Apr' => 0, 'May' => 0, 'June' => 0,
+            'July' => 6, 'Aug' => 4, 'Sept' => 4, 'Oct' => 10, 'Nov' => 4, 'Dec' => 8);
+        $profile->pto_usage = $months;
+        $profile->points_usage = $months;
+        $profile->resetMonths();
+
+        $reset = array('Jan' => 0, 'Feb' => 0, 'Mar' => 0, 'Apr' => 0, 'May' => 0, 'June' => 0,
+            'July' => 0, 'Aug' => 0, 'Sept' => 0, 'Oct' => 0, 'Nov' => 0, 'Dec' => 0);
+
+        $this->assertEquals($reset, $profile->pto_usage);
+        $this->assertEquals($reset, $profile->points_usage);
+        $this->assertGreaterThan($year_count, $profile->user->year_count);
     }
 }
