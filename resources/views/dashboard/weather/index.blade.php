529221e6b387aba388219099b3f9b88d86267724
phpdev <jvalentin0221@gmail.com>
2020-11-23 15:43:16 -0500

    Add DashboardWeather controller with index method

---
+++ resources/views/dashboard/weather/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('title', "Profile")
+
+@section('class', 'dashboard')
+
+@section('content', "WEATHER")
+
+@section('id', 'app')
+
+@section('body-header')
+    @include("layouts.navbar.dashboard")
+@endsection
+
+@section('body-content')
+    <div class="container-fluid">
+        <x-dash-sidebar/>
+
+        <div class="dashboard__main">
+            <div class="dashboard__main-content  dashboard__main-content--grey m-5">
+
+
+            </div>
+        </div>
+    </div>
+@endsection
