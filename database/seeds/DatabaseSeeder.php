a98c918123223aafebded9cd81f43e7f03619b66
phpdev <jvalentin0221@gmail.com>
2021-01-07 17:27:07 -0500

    Create new seeder to test database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(ActivitySeeder::class);
+         $this->call([
+             ProfileSeeder::class,
+         ]);
     }
 }
