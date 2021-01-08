4adecb40c20b27783f4802f79192f3e06e01711c
phpdev <jvalentin0221@gmail.com>
2021-01-08 13:35:50 -0500

    Create ActitityFactory to add to seeder

--- database/seeds/ActivitySeeder.php
+++ database/seeds/ActivitySeeder.php
@@ -14,19 +14,20 @@ class ActivitySeeder extends Seeder
      */
     public function run()
     {
+        factory(App\Activity::class, 50)->create();
         //Insert one entry to the Database
-        DB::table('activities')->insert([
-            'user_id' => 1,
-            'date' => '2020-11-08',
-            'time' => '09:35:47',
-            'pto_used'=> 4,
-            'points' =>1,
-            'pending' =>1,
-            'reason_for_request' =>'COVID',
-            'supervisor_name' => 'Joey',
-            'status' => 'accepted',
-            'updated_at' => '2020-12-15 21:32:41',
-            'created_at' => '2020-12-15 21:32:41'
-        ]);
+//        DB::table('activities')->insert([
+//            'user_id' => 1,
+//            'date' => '2020-11-08',
+//            'time' => '09:35:47',
+//            'pto_used'=> 4,
+//            'points' =>1,
+//            'pending' =>1,
+//            'reason_for_request' =>'COVID',
+//            'supervisor_name' => 'Joey',
+//            'status' => 'accepted',
+//            'updated_at' => '2020-12-15 21:32:41',
+//            'created_at' => '2020-12-15 21:32:41'
+//        ]);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
          $this->call([
              ProfileSeeder::class,
-             CalendarSeeder::class
+             CalendarSeeder::class,
+             ActivitySeeder::class
          ]);
     }
 }
