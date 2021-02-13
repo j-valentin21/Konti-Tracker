1c685918751d51dd0fc53d4469ec7ef3356fd2ed
phpdev <jvalentin0221@gmail.com>
2021-02-13 16:11:47 -0500

    Remove middleware on API controller

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -10,12 +10,6 @@ use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('NotFirstTimeUser');
-    }
-
     /**
      * Get weather data from API.
      *
