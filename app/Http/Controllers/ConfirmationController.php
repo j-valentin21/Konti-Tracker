a7373afdb95d65818a4d361364c9ad4c35cfeca9
phpdev <jvalentin0221@gmail.com>
2021-02-14 17:28:37 -0500

    Testing jenkins pipeline

--- app/Http/Controllers/ConfirmationController.php
+++ app/Http/Controllers/ConfirmationController.php
@@ -11,6 +11,7 @@ class ConfirmationController extends Controller
 {
     public function __construct()
     {
+        $this->middleware('auth');
         $this->middleware('firstTimeUser');
         $this->middleware('verified');
     }
