80620a4c8ce63a366e2c3c57296d765740235419
phpdev <jvalentin0221@gmail.com>
2021-02-17 15:02:29 -0500

    Add new test to ContactUsTest

--- tests/Feature/ContactUsTest.php
+++ tests/Feature/ContactUsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class ContactUsTest extends TestCase
@@ -13,5 +14,17 @@ class ContactUsTest extends TestCase
         $response = $this->get('/contact-us');
 
         $response->assertStatus(200);
+    }
+
+    public function test_User_Can_Send_Contact_Message()
+    {
+        $response = $this->post('/contact-us', [
+            'name' => 'James',
+            'email' => 'james@gmail.com',
+            'message' => 'This is a test message'
+        ]);
+
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect('/');
     }
 }
